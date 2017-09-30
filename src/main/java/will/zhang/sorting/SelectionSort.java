b43a497e7a74e95ffb15b824984c7ef7589c32ae
zhangjiawei <349071347@qq.com>
2017-09-30 18:57:37 +0800

    SelectionSort finished

--- src/main/java/will/zhang/sorting/SelectionSort.java
+++ src/main/java/will/zhang/sorting/SelectionSort.java
@@ -18,10 +18,9 @@ public class SelectionSort {
      */
     public static void sort(int[] arr){
         for (int i = 0; i < arr.length; i++) {
-            int min = arr[i];
             for (int j = i + 1; j < arr.length; j++) {
-                //遇到更小的则交换
-                if(arr[j] < min){
+                //遇到比arr[i]小的则交换
+                if(arr[j] < arr[i]){
                     swap(arr, i, j);
                 }
             }
@@ -41,7 +40,7 @@ public class SelectionSort {
     }
 
     public static void main(String[] args) {
-        int[] arr = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        int[] arr = {5, 9, 8, 7, 10, 6, 3, 0, 2, 1, 4};
         SelectionSort.sort(arr);
         System.out.println(Arrays.toString(arr));
     }
